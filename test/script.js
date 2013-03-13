f40f2e20f9993d84fdc8b3da031c06ac6b38f8bf
Suman Paul <sumank.paul@gmail.com>
2013-03-13 19:01:11 +0530

    test script update

--- test/script.js
+++ test/script.js
@@ -1,21 +1,21 @@
-// localStorage.clear();
-// lsd.createTable('humans');
-// data.forEach(function(_d) {
-// 	lsd.store('humans', _d);
-// });
+localStorage.clear();
+lsd.createTable('humans');
+data.forEach(function(_d) {
+	lsd.store('humans', _d);
+});
 
 console.time("index time");
-lsd.index('humans', 'country');
+lsd.index('humans', 'name');
 console.timeEnd("index time");
 
-var filter = {
-	f : 'country',
-	o:'=',
-	v:'China'
-};
-console.time("query time");
-var result = lsd.query('humans', filter);
-console.timeEnd("query time");
-result.forEach(function(val) {
-	console.log(val);
-});
+// var filter = {
+// 	f : 'country',
+// 	o:'=',
+// 	v:'China'
+// };
+// // console.time("query time");
+// var result = lsd.query('humans', filter);
+// // console.timeEnd("query time");
+// result.forEach(function(val) {
+// 	console.log(val);
+// });
