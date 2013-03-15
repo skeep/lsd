54ec0b47e825d539d7fc38ddda38b8769d4e4e90
Suman Paul <sumank.paul@gmail.com>
2013-03-15 19:47:53 +0530

    better documentation

--- test/script.js
+++ test/script.js
@@ -1,21 +1,33 @@
+//lets clear ourr database
 localStorage.clear();
+
+//create a new table of humans
 lsd.createTable('humans');
+
+//populate the data using store method
 data.forEach(function(_d) {
 	lsd.store('humans', _d);
 });
 
+
+//indexing time
 console.time("index time");
 lsd.index('humans', 'name');
 console.timeEnd("index time");
 
-// var filter = {
-// 	f : 'country',
-// 	o:'=',
-// 	v:'China'
-// };
-// // console.time("query time");
-// var result = lsd.query('humans', filter);
-// // console.timeEnd("query time");
-// result.forEach(function(val) {
-// 	console.log(val);
-// });
+//create your where clause
+var filter = {
+	f: 'name',
+	o: '=',
+	v: 'kim'
+};
+
+//query the table with given clause
+console.time("query time");
+var result = lsd.query('humans', filter);
+console.timeEnd("query time");
+
+//show time
+result.forEach(function(val) {
+	console.log(val);
+});
