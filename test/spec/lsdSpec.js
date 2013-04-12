8390265f761354463b7c307c7656fe4578e5a62a
Suman Paul <sumank.paul@gmail.com>
2013-04-12 15:16:02 +0530

    spec upadted

--- test/spec/lsdSpec.js
+++ test/spec/lsdSpec.js
@@ -1,6 +1,28 @@
-describe('LocalStorage', function(){
+describe('LocalStorage', function () {
 
-	it('should find lsd', function(){
+	it('should find lsd', function () {
 		expect(lsd.createDataStore('humans')).toBeTruthy();
 	});
+	var data = {
+		name: 'suman'
+	};
+	var storedData = lsd.set('humans', data);
+	it('should add data suman', function () {
+		expect(storedData.name).toEqual('suman');
+	});
+
+	var newData = {
+		id: storedData.id,
+		name: 'shreyashi'
+	};
+	var editedData = lsd.set('humans', newData);
+	it('should edit suman to shreyashi', function () {
+		expect(editedData.name).toEqual('shreyashi');
+	});
+
+	// it('should delete data', function () {
+	// 	var toBeDeleted = editedData.id;
+	// 	lsd.remove('humans', toBeDeleted);
+	// 	expect(lsd.get('humans', toBeDeleted)).toBeFalsy();
+	// });
 });
